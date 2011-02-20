97decf282fcc12cae3fa058d845742ea21463650
Narwhal <garrisonp3128@gmail.com>
2011-02-20 10:55:06 -0800

    Removed runtime exceptions

--- src/edu/wpi/first/wpilibj/templates/Logic/Disabled.java
+++ src/edu/wpi/first/wpilibj/templates/Logic/Disabled.java
@@ -12,28 +12,28 @@ public class Disabled extends ICPProtocol {
      * This method will be called initally
      */
     public void init(){
-        throw new RuntimeException("Not Implemented");
+
     }
 
     /*
      * This method will be called continously
      */
     public void continous(){
-        throw new RuntimeException("Not Implemented");
+
     }
 
     /*
      * This method will be called periodically
      */
     public void periodic(){
-        throw new RuntimeException("Not Implemented");
+
     }
 
     /*
      * Sets the Main Robot
      */
     public void setRobot(MainRobot r){
-        throw new RuntimeException("Not Implemented");
+
     }
 
 }
