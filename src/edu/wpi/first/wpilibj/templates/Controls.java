d8ad6d85d6f50517b9afe2039f572f9d747e791d
Tyler Carter <tyler@chacha102.com>
2011-02-21 11:32:27 -0800

    Fixed Pad to be a different type than stick

--- src/edu/wpi/first/wpilibj/templates/Controls.java
+++ src/edu/wpi/first/wpilibj/templates/Controls.java
@@ -8,6 +8,6 @@ public class Controls {
 
     public static final int BUTTON = 0;
     public static final int STICK = 1;
-    public static final int PAD = 1;
+    public static final int PAD = 2;
 
 }
